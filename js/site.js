015cc82a0f4f116612d12f4f76f7eaed8d3fbf04
Preytorkamen <101070557+Preytorkamen@users.noreply.github.com>
2023-01-25 20:15:56 -0500

    Javascript Complete

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+// Get the string from the page
+//Controller Function
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// Reverse the string
+//Logic Function
+function reverseString(userString){
+    let revString = [];
+
+    //Reverse string using for loop
+    for (let i = userString.length - 1; i >= 0; i--){
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+// Display the reversed string to the user
+function displayString(revString){
+    //Write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
